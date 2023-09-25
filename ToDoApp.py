8afcb5821b8306064162782c5d5183ed707fd9f0
tayleredelen <taylerledelen@gmail.com>
2023-09-25 13:11:21 -0500

    Bug fixes

--- ToDoApp.py
+++ ToDoApp.py
@@ -3,16 +3,16 @@ import functions
 
 todos = functions.get_todos()
 
-
 def add_todo():
-    todo = st.session_state["new_todo"] + '\n'
+    todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
 
-st.title("My ToDo App")
-st.subheader("This is my ToDo App")
-st.write("This app is to increase your productivity")
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,7 +22,5 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-st.text_input(label="", placeholder="Add a new ToDo...",
-              on_change=add_todo, key="new_todo")
-
-# st.session_state
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
--- functions.py
+++ functions.py
@@ -1,14 +1,21 @@
-def get_todos(filepath="/Users/tlede/Desktop/ToDoApp/files/todos.txt"):
-    # """ Read a text file and return a list
-    # of to-do list of items.
-    # """
+FILEPATH = "files/todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    """ Read a text file and return the list of
+    to-do items.
+    """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filepath="/Users/tlede/Desktop/ToDoApp/files/todos.txt"):
-    # """ Write to-do list items in text file."""
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
 
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
