6e4a4dfc28674530c93f967726061623c12cf8a3
tayleredelen <taylerledelen@gmail.com>
2023-09-26 11:57:12 -0500

    Final ToDo updates

--- ToDoApp.py
+++ ToDoApp.py
@@ -10,9 +10,10 @@ def add_todo():
 
 
 st.title("My Todo App")
-st.subheader("This is my todo app.")
+st.subheader("This is a ToDo app designed by Tayler Edelen.")
 st.write("This app is to increase your productivity.")
-
+st.write("Enter new ToDos in the box below.")
+st.write("Click the checkbox of a ToDo to complete it.")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
